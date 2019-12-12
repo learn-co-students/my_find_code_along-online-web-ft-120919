9a5ebdf25ddd9bfa27e50229cf41b05708a65548
Nina Angelo <ninacelie@gmail.com>
2019-12-12 02:19:52 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
-require 'pry'
-
 def my_find(collection)
+  i = 0 #added counter called i
+  while i < collection.length #while the counter is less than the array's length
+    if yield(collection[i]) #when code hits yield sends what's passed in as arg to block
+      return collection[i]
+    end
+    i = i + 1 #add an increment of 1
+  end
 
 end
